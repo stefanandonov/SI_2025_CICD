773be3b6d9cca33391496dfeaac89d1e8632705e
Ana Kostovska <ana.kostovska@students.finki.ukim.mk>
2026-05-12 19:56:41 +0200

    broken logic - tests should fail

--- src/main/java/app/ReverseWordsApp.java
+++ src/main/java/app/ReverseWordsApp.java
@@ -24,7 +24,8 @@ public class ReverseWordsApp {
         String[] words = sentence.split("\s+");
         StringBuilder result = new StringBuilder();
         for (int i=0;i<words.length;i++) {
-            result.append(new StringBuilder(words[i]).reverse()).append(" ");
+          result.append(words[i]).append(" "); 
+         //   result.append(new StringBuilder(words[i]).reverse()).append(" ");
         }
         return result.toString().trim();
     }
